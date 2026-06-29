17a6149c867f5ab82b4cadcd883f18ca259c5d86
WorldChallenge <rodrigocarbone01@gmail.com>
2026-06-29 18:23:03 -0300

    completed 067

--- exercises/067_comptime2.zig
+++ exercises/067_comptime2.zig
@@ -36,20 +36,19 @@ pub fn main() void {
     // In this contrived example, we've decided to allocate some
     // arrays using a variable count! But something's missing...
     //
-    var count = 0;
-
+    comptime var count = 0;
+    // @compileLog("Count at compile time: ", count);
     count += 1;
     const a1: [count]u8 = @splat('A');
-
+    // @compileLog("Count at compile time: ", count);
     count += 1;
     const a2: [count]u8 = @splat('B');
-
+    // @compileLog("Count at compile time: ", count);
     count += 1;
     const a3: [count]u8 = @splat('C');
-
+    // @compileLog("Count at compile time: ", count);
     count += 1;
     const a4: [count]u8 = @splat('D');
-
     print("{s} {s} {s} {s}\n", .{ a1, a2, a3, a4 });
 
     // Builtin BONUS!
@@ -61,5 +60,5 @@ pub fn main() void {
     //
     // Try uncommenting this line and playing around with it
     // (copy it, move it) to see what it does:
-    //@compileLog("Count at compile time: ", count);
+    // @compileLog("Count at compile time: ", count);
 }
