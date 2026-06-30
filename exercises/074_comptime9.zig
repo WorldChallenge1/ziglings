3bab5c887a1ae91dae527bdfa58ebfbd9459f2b3
WorldChallenge <rodrigocarbone01@gmail.com>
2026-06-29 21:07:43 -0300

    completed 074

--- exercises/074_comptime9.zig
+++ exercises/074_comptime9.zig
@@ -20,7 +20,6 @@ const Animal = enum {
 // know how big a pen we'll need) and a format string, like the "mlm" for
 // MouseLlama.
 fn makeCreature(comptime count: usize, comptime fmt: []const u8) [count]Animal {
-
     // Since not every animal is represented by a single character, we need to
     // track the state of things as we move along. For example, if we see an
     // "m", is that a new Mouse or the end of a Llama?
@@ -28,12 +27,12 @@ fn makeCreature(comptime count: usize, comptime fmt: []const u8) [count]Animal {
         start, // Ready to start a new animal.
         l, // This means we've seen an "l", so if we see an "m", we know it's a Llama.
     };
-    var state = State.start;
+    comptime var state = State.start;
 
     // We return an array of animals representing the creature. (This is why we
     // really needed the 'count' parameter. Arrays need a size.)
     var animals: [count]Animal = undefined;
-    var next_animal: usize = 0;
+    comptime var next_animal: usize = 0;
 
     inline for (fmt) |char| {
 
@@ -57,7 +56,10 @@ fn makeCreature(comptime count: usize, comptime fmt: []const u8) [count]Animal {
                 //
                 // What do you think happens with Gators? Do they join with
                 // other animals or is this an error?
-                'g' => ???,
+                'g' => {
+                    animals[next_animal] = .Gator;
+                    next_animal += 1;
+                },
 
                 else => @compileError(std.fmt.comptimePrint("No animal starts with '{c}'!", .{char})),
             },
@@ -69,7 +71,7 @@ fn makeCreature(comptime count: usize, comptime fmt: []const u8) [count]Animal {
                     next_animal += 1;
                     // Something is missing here. After we finish a Llama, we
                     // need to be ready to _start_ over with a new animal...
-                    ???
+                    state = .start;
                 },
 
                 else => @compileError("Only llamas start with 'l'!"),
